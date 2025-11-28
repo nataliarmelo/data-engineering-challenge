55e7b6f2d78c6924c35d71ecbcfe96c4e4088399
Natalia Melo <91853655+nahrdmelo@users.noreply.github.com>
2025-11-27 22:38:13 -0300

    finalização do etl dos dados brutos até disponiveis para uso

--- 02-academic_performance/src/etl_student_perf.py
+++ 02-academic_performance/src/etl_student_perf.py
@@ -43,7 +43,7 @@ numeric_cols = ["math_score", "reading_score", "writing_score"]
 for col in numeric_cols:
     df[col] = pd.to_numeric(df[col], errors="coerce")
 
-# drop linhas absurdas
+# drop linhas 
 df = df.dropna(subset=numeric_cols)
 
 
@@ -57,3 +57,34 @@ df_to_save = df.reset_index(drop=True).copy()
 df_to_save.to_parquet(str(PROCESS_PATH), engine="fastparquet", index=False)
 PROCESS_PATH
 print("Dados tratados salvos em:", PROCESS_PATH)
+
+
+### Métricas para uso na análise posterior
+
+# Criação de colunas com médias e status de aprovação
+df["avg_score"] = df[numeric_cols].mean(axis=1)
+df["passed"] = df["avg_score"] >= 60
+
+df_cur = df[[
+    "gender",
+    "race/ethnicity",
+    "parental_level_of_education",
+    "lunch",
+    "test_preparation_course",
+    "math_score",
+    "reading_score",
+    "writing_score",
+    "avg_score",
+    "passed"
+]]
+
+
+curated = Path('02-academic_performance/data/curated')
+curated.mkdir(parents=True, exist_ok=True)
+
+CURATED_PATH = curated / "students_performance.parquet"
+df_to_save = df.reset_index(drop=True).copy()
+
+df_to_save.to_parquet(str(CURATED_PATH), engine="fastparquet", index=False)
+CURATED_PATH
+print("Dados tratados salvos em:", CURATED_PATH)
